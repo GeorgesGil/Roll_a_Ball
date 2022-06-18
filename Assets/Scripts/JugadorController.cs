3e161dcc3f705f2831b5109d2c8ec6402aaf7352
GeorgesGil <103227730+GeorgesGil@users.noreply.github.com>
2022-06-17 21:03:22 -0400

    Add files via upload

---
+++ Assets/Scripts/JugadorController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JugadorController : MonoBehaviour
+{
+    
+    private Rigidbody rb;
+
+    private int contador;
+
+    public Text textoContador, textoGanar;
+
+    public float velocidad;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        
+        contador = 0;
+
+        setTextoContador(); 
+        
+        textoGanar.text = "";
+
+    }
+
+
+    void FixedUpdate()
+    {
+        float movimientoH = Input.GetAxis("Horizontal");
+        float movimientoV = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(movimientoH, 0.0f,movimientoV);
+
+        rb.AddForce(movimiento * velocidad);
+    }
+
+
+    void OnTriggerEnter(Collider other) 
+    { 
+        if (other.gameObject.CompareTag ("Coleccionable"))
+        { 
+            other.gameObject.SetActive (false); 
+
+            contador = contador + 1;
+
+            setTextoContador();
+        } 
+
+    } 
+
+
+    void setTextoContador()
+    { 
+        textoContador.text = "Contador: " + contador.ToString(); 
+        
+        if (contador >= 13)
+        { 
+            textoGanar.text = "¡Ganaste!"; 
+        } 
+        
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
